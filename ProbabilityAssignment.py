ecd2ed093ba30335593c9e2760ab345aab7fe4ad
Aprajita-cpu <78231261+Aprajita-cpu@users.noreply.github.com>
2021-02-16 23:41:47 +0530

    Add files via upload

---
+++ ProbabilityAssignment.py
@@ -0,0 +1,23 @@
+from random import randint
+from time import sleep
+
+heads=0
+tails=0
+print("Assignment 2 CS-808")
+print("Sample space={Head,Tail}")
+flip=int(input("How many times,you want to toss the coin:"))
+
+for i in range(flip):
+    result=randint(0,1)
+    sleep(1)
+    if result==0:
+        print("Heads")
+        heads += 1
+    else:
+        print("Tails")
+        tails +=1
+m=heads/flip
+n=tails/flip
+print("The results are:")
+print("Probability of Heads :",m)
+print("Probability of Tails :",n)
